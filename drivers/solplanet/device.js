6bee3b6d530b1cbd63125fb29ca9d2f40c627877
Maikel Reijnders <reijnders.maikel@gmail.com>
2025-11-04 08:30:52 +0100

    Added pac1 for current power

--- drivers/solplanet/device.js
+++ drivers/solplanet/device.js
@@ -76,38 +76,61 @@ class SolPlanet extends Inverter {
 						this.checksFailed = 0;
 						this.setDefaultInterval()
 					}
+
+					// FLG
+					const deviceState = _.parseInt( productionData.flg );
+					this.homey.log( `Current device state is: ${ deviceState }` );
+
+					if( deviceState !== 1 ) {
+						if( deviceState === 0 ) {
+							this.setValueWithCatch('meter_power', 0 );
+						}
+
+						return;
+					}
 					
 					// Temperature
 					const currentTemperature = Number( _.parseInt( productionData.tmp ) / 10 );
-					this.homey.log( `Current inverter temperature is ${ currentTemperature }` );
+					this.homey.log( `Current inverter temperature is: ${ currentTemperature }` );
 					
 					if( currentTemperature !== undefined ) {
-						this.setCapabilityValue( "measure_temperature", currentTemperature ).catch( this.onError.bind( this ) );
+						this.setValueWithCatch("measure_temperature", currentTemperature);
 					}
 					
 					// Current (w)
-					const currentProductionPower = Number( _.parseInt( productionData.pac ) );
-					this.homey.log( `Current production power is ${ currentProductionPower }W` );
+					let currentProductionPower = Number( _.parseInt( productionData.pac ) );
+					this.homey.log( `Current production power is: ${ currentProductionPower }W` );
+
+					// -> Check if pac1 exists
+					if( productionData.pac1 !== undefined ) {
+						const currentProductionPowerPac1 = Number( _.parseInt( productionData.pac1 ) );
+						this.homey.log( `Current production power PAC1 is: ${ currentProductionPowerPac1 }W` );
+
+						// Use pac1 if available
+						if( currentProductionPowerPac1 !== undefined ) {
+							currentProductionPower = currentProductionPowerPac1;
+						}
+					}
 					
 					// Ignore when the current production power is more than 20k?
 					if( currentProductionPower !== undefined && currentProductionPower <= 20000 ) {
-						this.setCapabilityValue( "measure_power", currentProductionPower ).catch( this.onError.bind( this ) );
+						this.setValueWithCatch("measure_power", currentProductionPower);
 					}
 					
 					// Daily (kWh)
 					const dailyProductionEnergy = Number( _.parseInt( productionData.etd ) / 10 );
-					this.homey.log( `Daily production energy is ${ dailyProductionEnergy }kWh` );
+					this.homey.log( `Daily production energy is: ${ dailyProductionEnergy }kWh` );
 					
 					if( dailyProductionEnergy !== undefined ) {
-						this.setCapabilityValue( "meter_power", dailyProductionEnergy ).catch( this.onError.bind( this ) );
+						this.setValueWithCatch("meter_power", dailyProductionEnergy);
 					}
 					
 					// Total (kWh)
 					const totalProductionEnergy = Number( _.parseInt( productionData.eto ) / 10 );
-					this.homey.log( `Total production energy is ${ totalProductionEnergy }kWh` );
+					this.homey.log( `Total production energy is: ${ totalProductionEnergy }kWh` );
 					
 					if( totalProductionEnergy !== undefined ) {
-						this.setCapabilityValue( "meter_power.total", totalProductionEnergy ).catch( this.onError.bind( this ) );
+						this.setValueWithCatch("meter_power.total", totalProductionEnergy);
 					}
 					
 					this.setAvailable().catch( this.onError.bind( this ) );
@@ -116,31 +139,24 @@ class SolPlanet extends Inverter {
 				// Log the error
 				this.onError( err );
 				
-				// Check if it is later than midnight
-				const now = new Date();
-				const midnight = new Date();
-				midnight.setHours(0,0,0,0);
-				
-				// And before 3 AM
-				const threeAm = new Date();
-				threeAm.setHours(3,0,0,0);
-				
-				if( now > midnight && now < threeAm ) {
-					// Update the daily production
-					this.setCapabilityValue( "meter_power", 0 ).catch( this.onError.bind( this ) );
-				}
-				
 				if( this.checksFailed > 3 ) {
 					// Change the interval to 5 minutes
 					this.resetInterval( 5 * 60 );
+
+					this.setValueWithCatch('meter_power', 0 );
 				}
 			}
 		} else if ( !this.api ) {
 			this.homey.log("SolPlanet could not be discovered on your network")
+
 			await this.setUnavailable(
 				"SolPlanet could not be discovered on your network"
 			);
 		}
+	}
+
+	setValueWithCatch( capabilityId, value ) {
+		this.setCapabilityValue( capabilityId, value ).catch( this.onError.bind( this ) );
 	}
 	
 	onError (error) {
